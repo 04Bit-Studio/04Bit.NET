7edff548cf9d865170b9be6b45bfcdce13482fb0
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2025-08-15 23:12:55 +0900

    [chore]: Add Runtime & Editor projects

---
+++ Editor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace O4Bit.Editor
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Editor");
+        }
+    }
+}
