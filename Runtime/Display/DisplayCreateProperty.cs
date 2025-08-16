c0cb4e153b3e33e7df58d152a0bc6c4a56a16f18
ASOBIBA <86977637+LHBM04@users.noreply.github.com>
2025-08-16 16:28:27 +0900

    [feature]: Create Display & Tests

---
+++ Runtime/Display/DisplayCreateProperty.cs
@@ -0,0 +1,17 @@
+﻿using Silk.NET.Maths;
+
+namespace Runtime.Display
+{
+    public record DisplayCreateProperty
+    {
+        public string Title;
+
+        public Vector2D<int> Position;
+
+        public Vector2D<int> Size;
+
+        public bool ShouldVSync;
+
+        public bool ShouldBorderless;
+    }
+}
